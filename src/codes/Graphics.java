1ace8fe0e698fad667f063e1644159741ed8a836
Peyton Seigo <pseigo@yahoo.com>
2017-06-13 00:59:31 -0600

    Change board text symbols

--- src/codes/Graphics.java
+++ src/codes/Graphics.java
@@ -1,36 +1,36 @@
 package codes;
 
 
-public class Graphics 
+public class Graphics
 {
-	
+
 	public static void displaySlots(Board slots)
 	{
 		System.out.println();
-		
+
 		for(int c = 0; c < slots.getColumns(); c++)
 			System.out.print((c+1) + " ");
-		
+
 		System.out.println();
 		System.out.println();
-		
+
 		for(int a = slots.getRows()-1; a >= 0; a--)
-		{		
+		{
 			for(int b = 0; b < slots.getColumns(); b++)
 			{
 				if(slots.getSlots(a, b) == 1)
-					System.out.print("*" + " ");
+					System.out.print("#" + " ");
 				else if (slots.getSlots(a, b) == 2)
-					System.out.print("^" + " ");
+					System.out.print("@" + " ");
 				else if (slots.getSlots(a, b) == 0)
-					System.out.print("~" + " ");
+					System.out.print("=-" + " ");
 			}
 				//System.out.print(slots.getSlots(a, b) + " ");
-			
+
 			System.out.println();
 		}
 	}
-	
+
 	public static void displayMessage(String message)
 	{
 		System.out.println();
